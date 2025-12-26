23778da9dbcae1fa18641dd17a3360dd9e10c382
dingky duo amurao <amurao.dingky.duo@asuene.com>
2025-12-26 16:02:35 +0800

    adjust a bit of linting and db config

--- src/main/java/com/example/order/repository/OrderRepository.java
+++ src/main/java/com/example/order/repository/OrderRepository.java
@@ -5,10 +5,3 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface OrderRepository extends JpaRepository<OrderEntity, String> {
 }
-package com.example.order.repository;
-
-import com.example.order.model.Order;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface OrderRepository extends JpaRepository<Order, Long> {
-}
